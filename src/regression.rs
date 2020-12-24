6caa11376ce07a42aaaac324473e35098c261bf7
Chubak <chubakbidpaa@gmail.com>
2020-12-24 19:46:57 +0330

    added log reg

--- src/regression.rs
+++ src/regression.rs
@@ -1,7 +1,8 @@
 mod util;
 use util::get_mean;
+use util::get_rand;
 
-struct LinReg {
+struct Reg {
     X: Vec<f64>,
     y: Vec<f64>,
     test_ratio: f64
@@ -12,16 +13,16 @@ trait LeastSquares {
     fn test_split(&self) -> (Vec<f64>, Vec<f64>, Vec<f64>, Vec<f64>);
     fn get_X_mean(&self) -> Option<f64>;
     fn get_Y_mean(&self) -> Option<f64>;
-    fn calculate_diff(&self, X_mean: f64, y_mean: f64) -> Option<f64>;
-    fn calculate_intercept(X_mean: f64, y_mean: f64, diff: f64) -> Option<f64>;
-    fn calculate_rmse(y_test: Vec<f64>, y_pred: Vec<f64>) -> Option<f64>;
+    fn calculate_diff(&self, X_mean: &f64, y_mean: &f64) -> Option<f64>;
+    fn calculate_intercept(X_mean: &f64, y_mean: f64, diff: &f64) -> Option<f64>;
+    fn calculate_rmse(y_test: &Vec<f64>, y_pred: &Vec<f64>) -> Option<f64>;
     fn fit(&self) -> (f64, f64, f64);
 }
 
-impl LeastSquares for LinReg {
+impl LeastSquares for Reg {
 
-    fn new(X: Vec<f64>, y: Vec<f64>, test_ratio: f64) -> LinReg {
-        LinReg {X: X, y: y, test_ratio: test_ratio}
+    fn new(X: Vec<f64>, y: Vec<f64>, test_ratio: f64) -> Reg {
+        Reg {X: X, y: y, test_ratio: test_ratio}
     }
 
     fn test_split(&self) -> (Vec<f64>, Vec<f64>, Vec<f64>, Vec<f64>) {
@@ -53,7 +54,7 @@ impl LeastSquares for LinReg {
 
     }
 
-    fn calculate_diff(&self, X_mean: f64, y_mean: f64) -> Option<f64> {
+    fn calculate_diff(&self, X_mean: &f64, y_mean: &f64) -> Option<f64> {
 
         let num = self.X.iter()
                     .zip(self.y.iter())
@@ -72,7 +73,7 @@ impl LeastSquares for LinReg {
         }
     }
 
-    fn calculate_intercept(&self, X_mean: f64, y_mean: f64, diff: f64) -> Option<f64> {
+    fn calculate_intercept(&self, X_mean: &f64, y_mean: &f64, diff: &f64) -> Option<f64> {
         
         match (X_mean == 0 || y_mean == 0) {
             true => None,
@@ -80,7 +81,7 @@ impl LeastSquares for LinReg {
         }
     }
 
-    fn calculate_rmse(y_test: Vec<f64>, y_pred: Vec<f64>) -> Option<f64> {
+    fn calculate_rmse(y_test: &Vec<f64>, y_pred: &Vec<f64>) -> Option<f64> {
 
         match (y_test.len() == y_pred.len()) {
             false => None,
@@ -101,3 +102,127 @@ impl LeastSquares for LinReg {
 }
 
 
+trait LogisticReg {
+    fn new(X: Vec<f64>, y: Vec<f64>, test_ratio: f64) -> Self;
+    fn test_split(&self) -> (Vec<f64>, Vec<f64>, Vec<f64>, Vec<f64>);
+    fn grad_descent(&self, x_in: &Vec<f64>, y_in: &Vec<f64>, learn_rate: &f64, init_weights: &Vec<f64>) -> Option<Vec<f64>>;
+    fn sigmoid(a: f64) -> Option<f64>;
+    fn stochastic_grad_descent(&self, x_in: &Vec<f64>, y_in: &Vec<f64>, max_cycles: i32, init_weights: &Vec<f64>) -> Option<Vec<f64>>;
+    fn classify(&self, x_in: &Vec<f64>, weights: &Vec<i32>) -> Option<f64>;
+    fn error_rate(y_pred: &Vec<f64>, y_test: &Vec<f64>) ->  Option<f64>;
+    fn train(&self);
+}
+
+impl LogisticReg for Reg {
+    fn new(X: Vec<f64>, y: Vec<f64>, test_ratio: f64) -> Reg {
+        Reg {X: X, y: y, test_ratio: test_ratio}
+    }
+
+    fn test_split(&self) -> (Vec<f64>, Vec<f64>, Vec<f64>, Vec<f64>) {
+        let len_appl = self.X.len() * self.test_ratio;
+
+        X_train = &self.X[..len_appl].as_vec();
+        X_test = &self.X[len_appl..].as_vec();
+
+        y_train = &self.y[..len_appl].as_vec();
+        y_test = &self.y[len_appl..].as_vec();
+
+        (X_train, X_test, y_train, y_test)
+
+    }
+
+    fn sigmoid(a: f64) {
+        1.0 / (1.0 + (-1 * a).exp())
+    }
+
+    fn grad_descent(&self, x_in: &Vec<f64>, y_in: &Vec<f64>, learn_rate: &f64, init_weights: &Vec<i32>) -> Option<Vec<i32>> {
+        if x_in.len() < 1 || init_weights.len() != x_in.len() || x_in.len() != y_in.len() {
+            None
+        }
+
+        let weights = init_weights.clone();
+        let alpha = learn_rate.clone()
+
+        for i in 0..x_in.len() {
+            let alpha = 4 / (1.0 + i) + alpha;
+            let logit_res = weights
+                                .iter()
+                                .zip(x_in.iter())
+                                .map(|(x, y)| self.sigmoid(x * y)?)
+                                .collect();
+            let error = y_in.iter()
+                                .zip(logit_res.iter())
+                                .map(|(x, y)| x  - y)
+                                .collect();
+            let descent = x_in.iter()
+                                .zip(error.iter())
+                                .map(|(x, y)| alpha * x * y)
+                                .collect()
+            let weights = weights.iter()
+                                .zip(descent.iter())
+                                .map(|x, y| x - y)
+                                .collect();
+        }        
+
+        Some(weights)
+    }
+
+    fn stochastic_grad_descent(&self, x_in: &Vec<f64>, y_in: &Vec<f64>, max_cycles: i32, init_weights: &Vec<f64>) -> Option<Vec<f64>> {
+        if x_in.len() < 1 || init_weights.len() != x_in.len() || x_in.len() != y_in.len() {
+            None
+        }
+
+        let weights: Vec<f64> = vec![1.00, x_in.len()];
+
+        for _ in 0..max_cycles {
+            let alpha = get_rand();
+
+            weights = self.grad_descent(x_in, y_in, &alpha, &weights)?;
+
+        }
+
+        Some(weights)
+
+
+    }
+
+    fn classify(&self, x_in: &Vec<f64>, weights: &Vec<i32>) -> Option<f64> {
+
+        if x_in.len() != weights.len() {
+            None
+        }
+
+        let prob = x_in.iter()
+                            .zip(weights.iter())
+                            .map(|(x, y)| self.sigmoid(x  y))
+                            .iter()
+                            .sum();
+
+        match (prob > 0.5) {
+            true => Some(1.0),
+            false => Some(0.0),
+        }
+
+    }
+
+    fn error_rate(y_pred: &Vec<f64>, y_test: &Vec<f64>) -> Option<f64> {
+
+        if y_pred.len() != y_test.len() {
+            None
+        }
+
+        let error_delta = y_pred.iter().zip(y_test.iter()).map(|(x, y)| x == y).collect();
+
+        let mut i = 0;
+        for error in error_delta {
+            if error {
+                i += 1;
+            }
+        }
+
+        i / y_test.len()
+
+    }
+
+
+}
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,11 @@
+use rand::Rng;
+
 pub fn get_mean(vec: Vec<f64>) -> f64 {
     vec.iter().sum::<f64>() / vec.len() as f64
+}
+
+pub fn get_rand() -> f64 {
+    let mut rng = rand::thread_rng();
+
+    rng.gen::<f64>();
 }
